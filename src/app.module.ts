b5ca505d482a7b1407781a096d4291da2fa2df83
Behruz <behruzbahtiyorov10@gmail.com>
2025-05-21 13:52:17 +0500

    fixed

--- src/app.module.ts
+++ src/app.module.ts
@@ -17,19 +17,19 @@ import { Department } from 'department/entities/department.entity';
     ConfigModule.forRoot(),
     ServeStaticModule.forRoot({
       rootPath: join(__dirname, '..', 'uploads'),
-      serveRoot: '/uploads'
+      serveRoot: '/uploads',
     }),
     TypeOrmModule.forRootAsync({
       useFactory: () => ({
         type: 'mysql',
-        host: process.env.HOST,
-        port: Number(process.env.DB_PORT),
-        username: process.env.DB_USER,
-        password: process.env.DB_PASSWORD,
-        database: process.env.DB_NAME,
+        host: 'mysql.railway.internal',
+        port: 3306,
+        username: 'root',
+        password: "MRTYBqIyYuTuPxzdnOSQTsUdXoYqPGft",
+        database: "railway",
         entities: [Drug, DrugArrival, DrugRequest, Department],
-        synchronize: true
-      })
+        synchronize: true,
+      }),
     }),
     DrugModule,
     DrugArrivalModule,
