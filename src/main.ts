fd02831cdf9762ad1cc7352d071e37a58a3fc665
Behruzbee <behruz.baxtiyorov.dev@gmail.com>
2026-04-08 20:31:31 +0500

    main

--- src/main.ts
+++ src/main.ts
@@ -6,25 +6,25 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   // Updated CORS configuration
-  app.enableCors({
-    origin: function (origin, callback) {
-      const allowedOrigins = [
-        'https://pas.orzumedical.uz',
-        'https://frontend-warehouse-orzumed.vercel.app',
-        'http://localhost:5173', // Не забудьте локалхост для удобной разработки!
-      ];
+  // app.enableCors({
+  //   origin: function (origin, callback) {
+  //     const allowedOrigins = [
+  //       'https://pas.orzumedical.uz',
+  //       'https://frontend-warehouse-orzumed.vercel.app',
+  //       'http://localhost:5173', // Не забудьте локалхост для удобной разработки!
+  //     ];
 
-      // Разрешаем запросы без origin (например, из Postman) или если origin есть в списке
-      if (!origin || allowedOrigins.includes(origin)) {
-        callback(null, true);
-      } else {
-        callback(new Error('Not allowed by CORS'));
-      }
-    },
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
-    credentials: true,
-    allowedHeaders: 'Content-Type, Accept, Authorization, x-api-key',
-  });
+  //     // Разрешаем запросы без origin (например, из Postman) или если origin есть в списке
+  //     if (!origin || allowedOrigins.includes(origin)) {
+  //       callback(null, true);
+  //     } else {
+  //       callback(new Error('Not allowed by CORS'));
+  //     }
+  //   },
+  //   methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+  //   credentials: true,
+  //   allowedHeaders: 'Content-Type, Accept, Authorization, x-api-key',
+  // });
 
   const PORT = process.env.PORT || 3001;
 
